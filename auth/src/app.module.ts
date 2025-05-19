5107702b3766c6038c9861b5c518fd79c9449d7a
pkk <psso2004@gmail.com>
2025-05-20 02:06:52 +0900

    feat: auth 불필요한 서비스, 컨트롤러 제거

--- auth/src/app.module.ts
+++ auth/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
@@ -13,7 +11,5 @@ import { UserModule } from './user/user.module';
     AuthModule,
     UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
