1db7f625d0ef1ddc8b4288a5896d84bdf39e6dd7
pkk <psso2004@gmail.com>
2025-05-19 21:50:34 +0900

    feat: joi, nestjs-joi 설치

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { EventModule } from './event/event.module';
+import { JoiPipeModule } from 'nestjs-joi';
 
 @Module({
   imports: [
@@ -12,6 +13,11 @@ import { EventModule } from './event/event.module';
     }),
     AuthModule,
     EventModule,
+    JoiPipeModule.forRoot({
+      pipeOpts: {
+        usePipeValidationException: true,
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { JoiPipe } from 'nestjs-joi';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new JoiPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
