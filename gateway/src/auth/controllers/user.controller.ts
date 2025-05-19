0e5c1a627065fa43d1b6e7c1a30a28eba99d53e6
pkk <psso2004@gmail.com>
2025-05-20 01:57:27 +0900

    feat: gateway strategy, guards, roles.decorator 추가

--- gateway/src/auth/controllers/user.controller.ts
+++ gateway/src/auth/controllers/user.controller.ts
@@ -1,18 +1,32 @@
-import { Body, Controller, Delete, Inject, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Inject,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthServiceClient } from 'src/grpc-client/protos/generated/auth';
 import { CreateUserInputDto } from '../dtos/inputs/create-user.input.dto';
 import { firstValueFrom } from 'rxjs';
 import { UserOutputDto } from '../dtos/outputs/user.output.dto';
 import { UpdateUserInputDto } from '../dtos/inputs/update-user.input.dto';
 import { DeleteUserInputDto } from '../dtos/inputs/delete-user.input.dto';
+import { JwtAuthGuard } from '../jwt-auth.guard';
+import { RolesGuard } from '../roles.guard';
+import { Roles } from '../decorators/roles.decorator';
+import { UserRole } from '../enums/user-role.enum';
 
 @Controller('user')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class UserController {
   constructor(
     @Inject('AUTH_PACKAGE_SERVICE')
     private readonly authService: AuthServiceClient,
   ) {}
 
+  @Roles(UserRole.ADMIN)
   @Post()
   async createUser(
     @Body() createUserDto: CreateUserInputDto,
@@ -23,6 +37,7 @@ export class UserController {
     return UserOutputDto.fromGrpcResponse(response);
   }
 
+  @Roles(UserRole.ADMIN)
   @Put()
   async updateUser(
     @Body() updateUserDto: UpdateUserInputDto,
@@ -33,6 +48,7 @@ export class UserController {
     return UserOutputDto.fromGrpcResponse(response);
   }
 
+  @Roles(UserRole.ADMIN)
   @Delete()
   async deleteUser(@Body() deleteUserDto: DeleteUserInputDto): Promise<void> {
     await firstValueFrom(this.authService.deleteUser(deleteUserDto));
---
+++ gateway/src/auth/jwt.strategy.ts
@@ -0,0 +1,22 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    const jwtSecret = process.env.JWT_SECRET;
+    if (!jwtSecret) {
+      throw new UnauthorizedException('JWT_SECRET is not defined');
+    }
+
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: jwtSecret,
+    });
+  }
+
+  async validate(payload: any) {
+    return { userId: payload.sub, email: payload.email, role: payload.role };
+  }
+}
