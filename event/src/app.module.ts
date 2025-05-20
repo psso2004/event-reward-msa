62196cc7c30b8cc87b9b73a23dc671f5da7204fc
pkk <psso2004@gmail.com>
2025-05-20 10:24:16 +0900

    feat: event grpc server 구현

--- event/src/app.module.ts
+++ event/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 
@@ -9,7 +8,6 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
   ],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- event/src/main.ts
+++ event/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'event',
+        protoPath: join(__dirname, 'protos', 'event.proto'),
+        url: `localhost:${process.env.PORT}`,
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
