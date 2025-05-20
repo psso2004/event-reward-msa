2db6c8361ac45bf3839391cf1cd0744b277ab1fd
pkk <psso2004@gmail.com>
2025-05-20 11:09:55 +0900

    feat: event server event, reward 모듈 추가

--- event/src/app.module.ts
+++ event/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { EventModule } from './event/event.module';
+import { RewardModule } from './reward/reward.module';
 
 @Module({
   imports: [
@@ -14,6 +16,8 @@ import { MongooseModule } from '@nestjs/mongoose';
         uri: configService.get<string>('MONGODB_URI'),
       }),
     }),
+    EventModule,
+    RewardModule,
   ],
   providers: [AppService],
 })
