7620277f14b19ed2647aac4040d1c9d52936c057
pkk <psso2004@gmail.com>
2025-05-20 11:04:17 +0900

    feat: event mongodb 연결

--- event/src/app.module.ts
+++ event/src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
     }),
   ],
   providers: [AppService],
